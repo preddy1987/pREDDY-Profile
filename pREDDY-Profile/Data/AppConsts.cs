dc22a85eaba6e92a6e69cb548f3f191659e06a40
Patrick Reddy <preddy@intellicure.com>
2022-07-25 14:34:00 -0400

    Add project files.

---
+++ pREDDY-Profile/Data/AppConsts.cs
@@ -0,0 +1,12 @@
+﻿namespace pREDDY_Profile.Data
+{
+    public static class AppConsts
+    {
+        public static class ExternalURLs
+        {
+            public static string LinkedIn = "https://www.linkedin.com/in/preddy1987/";
+            public static string GitHub = "https://github.com/preddy1987";
+                
+        }
+    }
+}
