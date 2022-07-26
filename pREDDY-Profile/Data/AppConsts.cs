354324b674e04fb8bd09b821e3d04df943d2fbf4
Patrick Reddy <preddy@intellicure.com>
2022-07-26 15:17:50 -0400

    updated index and added logos

--- pREDDY-Profile/Data/AppConsts.cs
+++ pREDDY-Profile/Data/AppConsts.cs
@@ -6,7 +6,12 @@
         {
             public static string LinkedIn = "https://www.linkedin.com/in/preddy1987/";
             public static string GitHub = "https://github.com/preddy1987";
-                
+            public static string Intellicure = "https://www.intellicure.com/";
+        }
+
+        public static class EmailAddresses 
+        {
+            public static string GMail = "patrickreddy87@gmail.com";
         }
     }
 }
--- pREDDY-Profile/Pages/Index.cshtml.cs
+++ pREDDY-Profile/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using pREDDY_Profile.Data;
 
 namespace pREDDY_Profile.Pages
 {
